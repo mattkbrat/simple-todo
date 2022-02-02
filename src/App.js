63591a7b1871ea798582107983d40c5816c13913
Matthew Bratrsovsky <mattkbrat@gmail.com>
2022-02-01 21:02:32 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -17,9 +19,42 @@ function App() {
         >
           Learn React
         </a>
+          <Task header="Hello" description="Description"/>
+          <Task header="Hello" description="Description"/>
+          <Task header="Hello" description="Description"/>
+
       </header>
     </div>
   );
+}
+
+class Task extends React.Component {
+    render() {
+        let description = this.props.description;
+        let header = this.props.header;
+        console.log(description)
+
+        return (
+            <div className="Task">
+                <h1>{header}</h1>
+                <p className="Task-description">{description}</p>
+                <div className="Task-buttons">
+                    <TaskButton buttonName="Delete"/>
+                    <TaskButton buttonName="Add"/>
+                </div>
+            </div>
+        )
+    }
+}
+
+class TaskButton extends React.Component{
+    render(){
+        let buttonName = this.props.buttonName
+        let button = <button
+            className="Task-button"
+            onClick={() => alert("I am an alert box!")} title={buttonName}>{buttonName}</button>
+        return (button)
+    }
 }
 
 export default App;
